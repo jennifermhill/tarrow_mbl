0d719ae92b157488907f412a664cc144b61e852d
Jennifer Hill <jennifer.hill@berkeley.edu>
2025-08-30 21:07:09 -0400

    Added data loader test script

---
+++ tests/test_data_loader.py
@@ -0,0 +1,15 @@
+# %%
+from tarrow.data import TarrowDataset
+
+dataset = TarrowDataset(imgs="/Volumes/sgrolab/jennifer/cryolite/cryolite_mixin_test65_2024-04-16/WS205_overnight_day2/2024-04-17_ERH_mixin65_plate2_WS205_overnight_day2_ERH Red FarRed/analysis/max_projections/maxz")
+
+img = dataset[0]
+img[0].shape
+
+# %%
+
+
+
+# %%
+
+# %%
